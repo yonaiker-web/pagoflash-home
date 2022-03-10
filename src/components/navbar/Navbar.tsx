cab7622cefdeae6aff0b21c8ed1df16f36562d67
Silvino <silvino.escalona@unidigital.global>
2022-03-10 12:22:56 -0400

    Cambiar NavBar a transparente

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -17,10 +17,12 @@ import {
   Text,
   useDisclosure,
 } from "@chakra-ui/react";
-import { NavLink } from "react-router-dom";
+import NavLink from "./NavLink";
 import { routes } from "../../routes/routes";
 import { Custom } from "../custom";
 import { AiOutlineUser } from "react-icons/ai";
+import { useScrollPosition } from "../../hooks/useScrollPosition";
+import { useRef, useState } from "react";
 
 interface INavLink {
   to: string;
@@ -29,100 +31,104 @@ interface INavLink {
 
 const links: INavLink[] = [
   { to: routes.home, text: "Inicio" },
-  { to: routes.home, text: "Productos" },
-  { to: routes.home, text: "Soporte" },
-  { to: routes.home, text: "Blog" },
-  { to: routes.home, text: "Contacto" },
-  { to: routes.home, text: "iniciar Sesión" },
+  { to: "/p", text: "Productos" },
+  { to: "/p", text: "Soporte" },
+  { to: "/p", text: "Blog" },
+  { to: "/p", text: "Contacto" },
+  { to: "/p", text: "Iniciar Sesión" },
 ];
 
 export const Navbar = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const ref = useRef<HTMLDivElement>(null);
+
+  const [bg, setBg] = useState("transparent");
   //const { user, signOut } = useAuth();
 
-  return (
-    <>
-      <Box bg={useColorModeValue("gray.100", "gray.900")} px={4}>
-        <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
-          <IconButton
-            size={"md"}
-            icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
-            aria-label={"Open Menu"}
-            display={{ md: "none" }}
-            onClick={isOpen ? onClose : onOpen}
-          />
-          <HStack spacing={8} alignItems={"center"}>
-            <Box>Logo</Box>
-          </HStack>
-          <Stack
-            flex={{ base: 1, md: 0 }}
-            justify={"flex-end"}
-            direction={"row"}
-            spacing={6}
-            whiteSpace="nowrap"
-            alignItems={"center"}
-            fontWeight="100"
-            fontSize={"xl"}
-          >
-            {/* <Button
-              as={"a"}
-              fontSize={"sm"}
-              fontWeight={400}
-              variant={"link"}
-              href={"#"}
-            >
-              Sign In
-            </Button>
-            <Button
-              display={{ base: "none", md: "inline-flex" }}
-              fontSize={"sm"}
-              fontWeight={600}
-              color={"white"}
-              bg={"pink.400"}
-              //href={"#"}
-              _hover={{
-                bg: "pink.300",
-              }}
-            >
-              Sign Up
-            </Button> */}
+  useScrollPosition(
+    ({ prevPos, currPos }) => {
+      console.log(currPos.y, window.screen.height);
 
+      if (currPos.y >= window.screen.height) {
+        setBg("black");
+      } else {
+        setBg("transparent");
+      }
+    },
+    undefined,
+    { useWindow: true }
+  );
+
+  return (
+    <Box
+      bg={bg}
+      px={4}
+      position="fixed"
+      left="0"
+      zIndex="50"
+      color="white"
+      width="full"
+      ref={ref}
+      transition="background 1s"
+    >
+      <Flex
+        alignItems={"center"}
+        justifyContent={"flex-end"}
+        maxW="1200px"
+        mx="auto"
+        py="4"
+      >
+        <IconButton
+          size={"md"}
+          icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+          aria-label={"Open Menu"}
+          display={{ md: "none" }}
+          onClick={isOpen ? onClose : onOpen}
+        />
+        {/* <HStack spacing={8} alignItems={"center"}>
+          <Box>Logo</Box>
+        </HStack> */}
+        <Stack
+          flex={{ base: 1, md: 0 }}
+          justify={"flex-end"}
+          direction={"row"}
+          spacing={6}
+          whiteSpace="nowrap"
+          alignItems={"center"}
+          fontWeight="medium"
+          fontSize="lg"
+        >
+          {links.map(({ to, text }, i) => (
+            <NavLink to={to} key={i}>
+              {text}
+            </NavLink>
+          ))}
+
+          <Button
+            display={{ base: "none", md: "inline-flex" }}
+            color={"white"}
+            bg={"orange.500"}
+            rounded="2px"
+            _hover={{
+              bg: "orange.300",
+            }}
+          >
+            Registrate
+          </Button>
+        </Stack>
+      </Flex>
+
+      {isOpen ? (
+        <Box pb={4} display={{ md: "none" }}>
+          <Stack as={"nav"} spacing={4}>
             {links.map(({ to, text }, i) => (
               <NavLink to={to} key={i}>
                 {text}
               </NavLink>
             ))}
-
-            <Button
-              display={{ base: "none", md: "inline-flex" }}
-              fontSize={"xl"}
-              fontWeight={100}
-              color={"white"}
-              bg={"orange.500"}
-              //href={"#"}
-              _hover={{
-                bg: "orange.300",
-              }}
-            >
-              Registro
-            </Button>
           </Stack>
-        </Flex>
-
-        {isOpen ? (
-          <Box pb={4} display={{ md: "none" }}>
-            <Stack as={"nav"} spacing={4}>
-              {links.map(({ to, text }, i) => (
-                <NavLink to={to} key={i}>
-                  {text}
-                </NavLink>
-              ))}
-            </Stack>
-          </Box>
-        ) : null}
-      </Box>
-
-      <Box p={4}>Main Content Here</Box>
-    </>
+        </Box>
+      ) : null}
+    </Box>
   );
 };
